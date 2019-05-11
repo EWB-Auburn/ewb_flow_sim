d98770fbac1da0a381b1d164d03f376d17786e83
Christian Moomaw <element15@phosfor.us>
2019-05-11 16:23:21 +0200

    Modify reynolds() and haaland_friction_factor() to work with lists of values

--- haaland_friction_factor.m
+++ haaland_friction_factor.m
@@ -1,6 +1,6 @@
 % Calculate the friction factor using the Haaland approximation for a given
 % Reynolds number and relative roughness
 function f = haaland_friction_factor(reynolds_number, relative_roughness)
-f = (-1.8 * log10( ...
-    (relative_roughness / 3.7)^1.11 + (6.9 / reynolds_number)))^-2;
+f = (-1.8 .* log10( ...
+    (relative_roughness ./ 3.7).^1.11 + (6.9 ./ reynolds_number))).^-2;
 end
